8a593fb1c173f2ffd867358b27c15740457d8d0a
黄毅圆 <78136558+qsjhyy@users.noreply.github.com>
2021-10-20 16:53:42 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
 let result = 0
-while (!(ASR.begin(ASR.ModeEnum.LOOP, ASR.MicrophoneModeEnum.MIC))) {
+while (!(ASR.begin(ASR.ModeEnum.BUTTON, ASR.MicrophoneModeEnum.MIC))) {
     serial.writeLine("Communication with device failed, please check connection")
     basic.pause(2000)
 }
 serial.writeLine("Begin ok!")
+if (ASR.addCommand("xiao zhi", 0)) {
+    serial.writeLine("Set key words xiao zhi succeed!")
+}
 if (ASR.addCommand("kai deng", 1)) {
     serial.writeLine("Set key words kai deng succeed!")
 }
@@ -23,10 +26,10 @@ led.enable(false)
 basic.forever(function () {
     result = ASR.read()
     if (1 == result) {
-        serial.writeLine("received'kai deng',command flag'0'")
+        serial.writeLine("received'kai deng',command flag'1'")
         led.enable(true)
     } else if (2 == result) {
-        serial.writeLine("received'guan deng',command flag'1'")
+        serial.writeLine("received'guan deng',command flag'2'")
         led.enable(false)
     }
     basic.pause(200)
